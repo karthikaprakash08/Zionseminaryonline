48117f46cd42fc83584e01c4b85fcbdaddceae80
Ashwoq_s_ <ashwoq00786@gmail.com>
2024-08-28 19:38:52 +0530

    profile

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,5 +1,20 @@
-function App() {
-  return <>Hai</>;
-}
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Profile from "./Components/Profile/Profile";
+import Dashboard from "./Components/Dashboard";
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Dashboard />}>
+            {/* <Route path="" index element={<Home />}></Route> */}
+            <Route path="profile" index element={<Profile />}></Route>
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
 export default App;

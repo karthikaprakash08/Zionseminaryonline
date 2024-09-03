162a8184cd7b7a9e36a8312ab880b6ccc327cf14
PRINCE ROY <83387075+princeroy8606@users.noreply.github.com>
2024-09-03 12:12:01 +0530

    add -> navigation to admin

--- frontEnd/src/Authentication/Register.jsx
+++ frontEnd/src/Authentication/Register.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './Register.css'
 import { useForm } from 'react-hook-form';
 import Select from 'react-select';
+import { useNavigate, useNavigation } from 'react-router-dom';
+
 
 const options = {
   maritalStatus: [
@@ -35,111 +37,113 @@ const options = {
 
 function Register() {
 
-    const { register, handleSubmit, setValue } = useForm();
-  
+  const navigate = useNavigate()
+  const { register, handleSubmit, setValue } = useForm();
+
   const onSubmit = (data) => {
     console.log(data);
+
   };
 
 
   return (
     <div className='register'>
       <form className="my-form" onSubmit={handleSubmit(onSubmit)}>
-      <h2 style={
-        {
-          width:'100%',
-          textAlign: 'center',
-          marginBottom:"10px"
-        }
-      }>Registration form</h2>
-      <div className="form-container">
-        <div className="leftcolumn">
-          <div className="form-group">
-            <label>First Name *</label>
-            <input type="text" placeholder="Enter First Name" {...register('firstName', { required: true })} />
+        <h2 style={
+          {
+            width: '100%',
+            textAlign: 'center',
+            marginBottom: "10px"
+          }
+        }>Registration form</h2>
+        <div className="form-container">
+          <div className="leftcolumn">
+            <div className="form-group">
+              <label>First Name *</label>
+              <input type="text" placeholder="Enter First Name" {...register('firstName', { required: true })} />
+            </div>
+            <div className="form-group">
+              <label>Last Name</label>
+              <input type="text" placeholder="Enter Last Name" {...register('lastName')} />
+            </div>
+            <div className="form-group">
+              <label>Mobile No *</label>
+              <input type="tel" placeholder="Enter Mobile Number" {...register('mobileNo', { required: true })} />
+            </div>
+            <div className="form-group">
+              <label>Email *</label>
+              <input type="email" placeholder="Enter email address" {...register('email', { required: true })} />
+            </div>
+            <div className="form-group">
+              <label>Marital Status *</label>
+              <Select
+                options={options.maritalStatus}
+                onChange={(option) => setValue('maritalStatus', option.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label>DOB *</label>
+              <input type="date" {...register('dob', { required: true })} />
+            </div>
+            <div className="form-group">
+              <label>Gender *</label>
+              <Select
+                options={options.gender}
+                onChange={(option) => setValue('gender', option.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label>Applying for *</label>
+              <Select
+                options={options.applyingFor}
+                onChange={(option) => setValue('applyingFor', option.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label>Educational Qualification *</label>
+              <input type="text" placeholder="Enter Educational Qualification" {...register('educationalQualification', { required: true })} />
+            </div>
           </div>
-          <div className="form-group">
-            <label>Last Name</label>
-            <input type="text" placeholder="Enter Last Name" {...register('lastName')} />
-          </div>
-          <div className="form-group">
-            <label>Mobile No *</label>
-            <input type="tel" placeholder="Enter Mobile Number" {...register('mobileNo', { required: true })} />
-          </div>
-          <div className="form-group">
-            <label>Email *</label>
-            <input type="email" placeholder="Enter email address" {...register('email', { required: true })} />
-          </div>
-          <div className="form-group">
-            <label>Marital Status *</label>
-            <Select
-              options={options.maritalStatus}
-              onChange={(option) => setValue('maritalStatus', option.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label>DOB *</label>
-            <input type="date" {...register('dob', { required: true })} />
-          </div>
-          <div className="form-group">
-            <label>Gender *</label>
-            <Select
-              options={options.gender}
-              onChange={(option) => setValue('gender', option.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label>Applying for *</label>
-            <Select
-              options={options.applyingFor}
-              onChange={(option) => setValue('applyingFor', option.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label>Educational Qualification *</label>
-            <input type="text" placeholder="Enter Educational Qualification" {...register('educationalQualification', { required: true })} />
+          <div className="rightcolumn">
+            <div className="form-group">
+              <label>Theological Qualification *</label>
+              <input type="text" placeholder="Enter Theological Qualification" {...register('theologicalQualification', { required: true })} />
+            </div>
+            <div className="form-group">
+              <label>Present Address *</label>
+              <input type="text" placeholder="Enter Present Address" {...register('presentAddress', { required: true })} />
+            </div>
+            <div className="form-group">
+              <label>Ministry Experience *</label>
+              <input type="text" placeholder="Enter Ministry Experience" {...register('ministryExperience', { required: true })} />
+            </div>
+            <div className="form-group">
+              <label>Salvation Experience *</label>
+              <input type="text" placeholder="Enter Salvation Experience" {...register('salvationExperience', { required: true })} />
+            </div>
+            <div className="form-group">
+              <label>Signature *</label>
+              <input type="file" {...register('signature', { required: true })} />
+            </div>
+            <div className="form-group">
+              <label>Passport Size Photo *</label>
+              <input type="file" {...register('passportSizePhoto', { required: true })} />
+            </div>
+            <div className="form-group">
+              <label>Education Certificate *</label>
+              <input type="file" {...register('educationCertificate', { required: true })} />
+            </div>
+            <div className="form-group">
+              <label>User Name *</label>
+              <input type="text" placeholder="Enter username" {...register('userName', { required: true })} />
+            </div>
+            <div className="form-group">
+              <label>Password *</label>
+              <input type="password" placeholder="Enter password" {...register('password', { required: true })} />
+            </div>
           </div>
         </div>
-        <div className="rightcolumn">
-          <div className="form-group">
-            <label>Theological Qualification *</label>
-            <input type="text" placeholder="Enter Theological Qualification" {...register('theologicalQualification', { required: true })} />
-          </div>
-          <div className="form-group">
-            <label>Present Address *</label>
-            <input type="text" placeholder="Enter Present Address" {...register('presentAddress', { required: true })} />
-          </div>
-          <div className="form-group">
-            <label>Ministry Experience *</label>
-            <input type="text" placeholder="Enter Ministry Experience" {...register('ministryExperience', { required: true })} />
-          </div>
-          <div className="form-group">
-            <label>Salvation Experience *</label>
-            <input type="text" placeholder="Enter Salvation Experience" {...register('salvationExperience', { required: true })} />
-          </div>
-          <div className="form-group">
-            <label>Signature *</label>
-            <input type="file" {...register('signature', { required: true })} />
-          </div>
-          <div className="form-group">
-            <label>Passport Size Photo *</label>
-            <input type="file" {...register('passportSizePhoto', { required: true })} />
-          </div>
-          <div className="form-group">
-            <label>Education Certificate *</label>
-            <input type="file" {...register('educationCertificate', { required: true })} />
-          </div>
-          <div className="form-group">
-            <label>User Name *</label>
-            <input type="text" placeholder="Enter username" {...register('userName', { required: true })} />
-          </div>
-          <div className="form-group">
-            <label>Password *</label>
-            <input type="password" placeholder="Enter password" {...register('password', { required: true })} />
-          </div>
-        </div>
-        </div>
-        <button type="submit" className="submit-btn">Submit</button>
+        <button type="submit" className="submit-btn" onClick={() => navigate('/admin')}>Submit</button>
       </form>
     </div>
   )
